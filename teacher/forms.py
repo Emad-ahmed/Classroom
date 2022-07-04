57f9c46457b4e611ce05f4b4fd8c1c2148e5d401
Emad-ahmed <amadahmed1234678@gmail.com>
2022-07-04 00:49:11 -0700

    chnges

--- teacher/forms.py
+++ teacher/forms.py
@@ -1,4 +1,5 @@
 from cProfile import label
+from urllib import request
 from django.contrib.auth.models import User
 from django import forms
 from django.forms import fields, widgets
@@ -17,10 +18,23 @@ class CreateClassForm(forms.ModelForm):
         labels = {"class_code_name": "Subject Code"}
 
         widgets = {
-            'class_code_name': forms.TextInput(attrs={'placeholder': 'Class Code'}),
+            'class_code_name': forms.TextInput(attrs={'placeholder': 'Subject Code'}),
             'section': forms.TextInput(attrs={'placeholder': 'Section'}),
             'subject_name': forms.TextInput(attrs={'placeholder': 'Subject name'}),
-            'room': forms.EmailInput(attrs={'placeholder': 'Room'}),
-            'classcode': forms.PasswordInput(attrs={'placeholder': 'Code'}),
-
+            'room': forms.TextInput(attrs={'placeholder': 'Room'}),
+            'classcode': forms.TextInput(attrs={'placeholder': 'Code'}),
         }
+
+    def clean_classcode(self):
+        classcode = self.cleaned_data['classcode']
+        if CreateClass.objects.filter(classcode=classcode).exists():
+            raise forms.ValidationError("Class Code Already Exists")
+
+        return classcode
+
+    def clean_class_code_name(self):
+        class_code_name = self.cleaned_data['class_code_name']
+        if (CreateClass.objects.filter(class_code_name=class_code_name).exists()) and (CreateClass.objects.filter(user=request.user).exists()):
+            raise forms.ValidationError("Subject Code Already Exitst")
+
+        return class_code_name
--- teacher/views.py
+++ teacher/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from .models import CreateClass
 from .forms import CreateClassForm
+from django.contrib import messages
 # Create your views here.
 
 
 class TeacherHome(View):
     def get(self, request):
-        return render(request, 'teacher_home.html')
+        if request.user.is_authenticated:
+            create = CreateClass.objects.filter(user=request.user)
+            return render(request, 'teacher_home.html', {'create': create})
+        else:
+            return redirect("/")
 
 
 class CreateClassview(View):
     def get(self, request):
         fm = CreateClassForm()
+        return render(request, 'createclass.html', {'form': fm})
+
+    def post(self, request):
+        fm = CreateClassForm(request.POST)
+        if fm.is_valid():
+            obj = fm.save(commit=False)
+            obj.user = request.user
+            obj.save()
+            messages.success(request, "Successfully Saved")
+        else:
+            messages.success(request, "Failed To Saved")
         return render(request, 'createclass.html', {'form': fm})
